94c0ebe5ac785231338f6950bea490453725feba
Christoph Becker <tuergeist@gmail.com>
2018-10-29 21:39:21 +0100

    test

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import cherrypy
 import os
+import psycopg2
 from jinja2 import Environment, FileSystemLoader
+
 env = Environment(loader=FileSystemLoader('html'))
 
-import psycopg2
 DATABASE_URL = os.environ['DATABASE_URL']
+TELEGRAM_BOT = os.environ['TELEGRAM_BOT']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 cur.execute("""
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users (
     id integer PRIMARY KEY,
     username text
 )
@@ -17,12 +19,16 @@ CREATE TABLE users(
 conn.commit()
 
 
-class HelloWorld(object):
+class MessageSender(object):
     @cherrypy.expose
     def index(self):
         data_to_show = ['Hello', 'world']
         tmpl = env.get_template('index.html')
         return tmpl.render(data=data_to_show)
+
+    @cherrypy.expose
+    def test(self):
+        return 'test'
 
 config = {
     'global': {
@@ -36,4 +42,4 @@ config = {
     }
 }
 
-cherrypy.quickstart(HelloWorld(), '/', config=config)
+cherrypy.quickstart(MessageSender(), config=config)
